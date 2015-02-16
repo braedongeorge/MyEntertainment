dbe7b9517445dc68a68aca8066103d15eb3b4656
braedongeorge <braedonreid@gmail.com>
2015-02-16 19:22:12 +1000

    Recovered Files

--- MyEntertainment/Global.asax.cs
+++ MyEntertainment/Global.asax.cs
@@ -14,7 +14,7 @@ namespace MyEntertainment
         protected void Application_Start()
         {
             Database.SetInitializer<Models.ApplicationDbContext>(null);
-            AreaRegistration.RegisterAllAreas();
+            AreaRegistration.RegisterAllAreas(); 
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
